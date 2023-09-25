04f52898219a616757a09340f754e96bd2ed7cfc
Elizeu Lima <elizeucoca@gmail.com>
2023-09-25 18:19:26 -0300

    build

--- src/components/dataTable/DataTable.tsx
+++ src/components/dataTable/DataTable.tsx
@@ -5,7 +5,7 @@ import {
 } from "@mui/x-data-grid";
 import "./dataTable.scss";
 import { Link } from "react-router-dom";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
+
 
 type Props = {
   columns: GridColDef[];
