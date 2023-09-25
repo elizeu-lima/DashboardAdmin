be0e812a3da62372f0e38ccaa9b6cd0d7dc21a42
Elizeu Lima <elizeucoca@gmail.com>
2023-09-25 18:05:44 -0300

    first commit

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+     <span>EliAdimin</span>
+     <span>© Dashboard Admin</span>
+    </div>
+  )
+}
+
+export default Footer
